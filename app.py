8a1534fc02d5d1dbf7fab3a437506a6a458cc457
Jeremy Cope <j.cope@me.com>
2018-03-13 14:55:10 -0400

    Removed Latex Attempt

--- app.py
+++ app.py
@@ -25,58 +25,6 @@ app.secret_key = 's3cr3t'
 #Default
 selected_datasets = ['LUX_2016_SI', 'CMS_monojet_July2017_VECTOR']
 
-JS_CODE = """
-import {Label, LabelView} from "models/annotations/label"
-
-export class LatexLabelView extends LabelView
-  render: () ->
-
-    #--- Start of copied section from ``Label.render`` implementation
-
-    # Here because AngleSpec does units tranform and label doesn't support specs
-    switch @model.angle_units
-      when "rad" then angle = -1 * @model.angle
-      when "deg" then angle = -1 * @model.angle * Math.PI/180.0
-
-    panel = @model.panel ? @plot_view.frame
-
-    xscale = @plot_view.frame.xscales[@model.x_range_name]
-    yscale = @plot_view.frame.yscales[@model.y_range_name]
-
-    sx = if @model.x_units == "data" then xscale.compute(@model.x) else panel.xview.compute(@model.x)
-    sy = if @model.y_units == "data" then yscale.compute(@model.y) else panel.yview.compute(@model.y)
-
-    sx += @model.x_offset
-    sy -= @model.y_offset
-
-    #--- End of copied section from ``Label.render`` implementation
-
-    # Must render as superpositioned div (not on canvas) so that KaTex
-    # css can properly style the text
-    @_css_text(@plot_view.canvas_view.ctx, "", sx, sy, angle)
-
-    # ``katex`` is loaded into the global window at runtime
-    # katex.renderToString returns a html ``span`` element
-    katex.render(@model.text, @el, {displayMode: true})
-
-export class LatexLabel extends Label
-  type: 'LatexLabel'
-  default_view: LatexLabelView
-"""
-class LatexLabel(Label):
-    """A subclass of the Bokeh built-in `Label` that supports rendering
-    LaTex using the KaTex typesetting library.
-
-    Only the render method of LabelView is overloaded to perform the
-    text -> latex (via katex) conversion. Note: ``render_mode="canvas``
-    isn't supported and certain DOM manipulation happens in the Label
-    superclass implementation that requires explicitly setting
-    `render_mode='css'`).
-    """
-    __javascript__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"]
-    __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
-    __implementation__ = JS_CODE
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -143,19 +91,6 @@ def index():
     p1.xaxis.axis_label_text_font_size = "14pt"
     p1.yaxis.axis_label_text_font_size = "14pt"
 
-    '''
-    x = np.arange(0.0, 1.0 + 0.01, 0.01)
-    y = np.cos(2*2*np.pi*x) + 2
-
-    p1 = figure(title="LaTex Demonstration", plot_width=500, plot_height=500)
-    p1.line(x, y)
-    latex = LatexLabel(text="f = \sum_{n=1}^\infty\\frac{-e^{i\pi}}{2^n}!",
-                        x=35, y=445, x_units='screen', y_units='screen',
-                        render_mode='css', text_font_size='16pt',
-                        background_fill_color='#ffffff')
-
-    p1.add_layout(latex)
-    '''
     p2 = figure(
         title='Collider Conversion',
         tools='wheel_zoom, pan, save',
